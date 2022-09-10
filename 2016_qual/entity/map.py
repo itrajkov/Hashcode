73858f8361a198c725938de09e653ecfb0283461
Ivan Trajkov <ivantrajkov@Ivans-MacBook-Pro.local>
2022-09-10 15:29:00 +0200

    Add numpy and write Map class to initialize a 2D numpy array.

--- 2016_qual/entity/map.py
+++ 2016_qual/entity/map.py
@@ -1,3 +1,25 @@
+import numpy as np
+
 class Map:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, r, c) -> None:
+        self.r = r # number of rows
+        self.c = c # number of columns
+        self._cells = np.zeros((r,c), dtype=int) # numpy array of cells
+        print("Cells initialized.")
+
+    def set_cell(self, r, c, v) -> None:
+        self._cells[r,c] = v
+
+    def __repr__(self):
+        for i in range(self.r):
+            for j in range(self.c):
+                print(self._cells[i,j], end=" ")
+            print()
+
+    def __str__(self) -> str:
+        s = ""
+        for i in range(self.r):
+            for j in range(self.c):
+                s += f"{self._cells[i,j]} "
+            s += "\n"
+        return s
--- 2016_qual/main.py
+++ 2016_qual/main.py
@@ -1,7 +1,9 @@
 import entity
 
 def main():
+    map = entity.Map(10, 10)
     print("Simulation initialized..")
+    print(map)
 
 if __name__ == "__main__":
     main()
